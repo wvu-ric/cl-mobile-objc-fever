01e9d636e86d81560d4882a2007d7fd80dfcf6f5
rwatalin <rwatalin@mix.wvu.edu>
2016-02-09 17:01:28 -0500

    Completed fever assignment RW

--- Fever/Fever/main.m
+++ Fever/Fever/main.m
@@ -16,10 +16,17 @@ int main(int argc, const char * argv[]) {
         
         // Use if-else logic to print out the correct response:
         
-        NSLog(@"You're running a bit cool.");
-        NSLog(@"You've got the fever... THE OBJECTIVE-C FEVER.");
-        NSLog(@"You'll be alright kid.");
-    
+        if (userTemperature > 100) {
+            NSLog(@"You've got the fever... THE OBJECTIVE-C FEVER.");
+        }
+        else if (userTemperature < 90) {
+            NSLog(@"You're running a bit cool.");
+        }
+        else {
+            NSLog(@"You'll be alright kid.");
+        }
+        
     }
+    
     return 0;
 }
